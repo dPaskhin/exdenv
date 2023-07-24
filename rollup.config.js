bb8cabfd08414511b9060d38c705df6c4f6cb088
Dmitrii Paskhin <d.pasxin@gmail.com>
2023-07-25 03:05:27 +0400

    feat(building): add minifier (terser)

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const terser = require('@rollup/plugin-terser');
 
 module.exports = {
   input: './lib/index.js',
@@ -7,5 +8,5 @@ module.exports = {
     file: './lib/index.js',
     format: 'cjs',
   },
-  plugins: [nodeResolve()],
+  plugins: [nodeResolve(), terser()],
 };
